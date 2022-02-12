76da0772cf54d787bd573cd038534151101a20e7
cleiton.leonel@gmail.com <cleiton.leonel@gmail.com>
2022-02-12 20:15:21 -0300

    ‘Upgrade’ no instalador versão 2022

--- start_project.sh
+++ start_project.sh
@@ -1,16 +1,37 @@
-#!/bin/bash
+#!/bin/bash -x
+#
+COMMAND=$1
 
 # Definindo o path do projeto
-echo 'Path do projeto'
 project_system='melinux_web'
 cd ~/${project_system} || return
 
 # Iniciando virtualenv
-env=~/venv_melinux/bin/activate
+env=~/venvs/venv_melinux/bin/activate
 echo 'Ativando ambiente virtual'
 source ${env}
 
-# Starting projeto
-py=~/venv_melinux/bin/python3
-run="manage.py runserver 0.0.0.0:9000"
-${py} ${run}
+if [ "$COMMAND" = "runserver" ]
+then
+  python manage.py runserver 0.0.0.0:9000
+fi
+if [ "$COMMAND" = "pip_uninstall" ]
+then
+  python manager_pip.py uninstall
+fi
+if [ "$COMMAND" = "pip_install" ]
+then
+  python manager_pip.py install
+fi
+if [ "$COMMAND" = "makemigrations" ]
+then
+  python manage.py makemigrations
+fi
+if [ "$COMMAND" = "migrate" ]
+then
+  python manage.py migrate
+fi
+if [ "$COMMAND" = "db_clean" ]
+then
+  python manage.py db_clean authentication entities communications security commons products commands
+fi
